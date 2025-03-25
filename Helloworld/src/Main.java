5c0bd62886f314fc3ece9da021a18e7e852bf512
kms0802 <kms0802@cju.ac.kr>
2025-03-25 11:29:13 +0900

    25.03.25 화

---
+++ Helloworld/src/Main.java
@@ -0,0 +1,29 @@
+public class Main {
+    public static void main(String[] args) {
+       String name = "김민서";
+       int age = 25;
+
+
+       System.out.print(name);
+       System.out.print("님은 ");
+       System.out.print(age);
+       System.out.print("살 입니다.\n");
+
+       System.out.println(name + "님은" + age + "살 입니다.");
+
+       double PI = 3.14159265358979;
+       double HalfZr = 10;
+
+       double rPtks = PI * HalfZr * HalfZr;
+
+       System.out.println(rPtks);
+
+
+
+
+
+
+
+
+    }
+}
