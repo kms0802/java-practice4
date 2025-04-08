5228e889ff9963155ebbbef615fafc978c0359db
kms0802 <kms0802@cju.ac.kr>
2025-04-08 23:43:44 +0900

    오버라이딩 오버로딩 상속 연관문제풀이

--- VSCode_Java/src/OPPs/Overloading_and_Overriding/Overloading_and_Overriding_Challenges/Overloading_and_Overriding_Challenge_2.java
+++ VSCode_Java/src/OPPs/Overloading_and_Overriding/Overloading_and_Overriding_Challenges/Overloading_and_Overriding_Challenge_2.java
@@ -1,5 +1,41 @@
 package OPPs.Overloading_and_Overriding.Overloading_and_Overriding_Challenges;
 
+class MusicPlayer {
+    public void play(String song) {
+        System.out.println("[기본 플레이어] 재생 중: " + song);
+    }
+    public void play(String song, int repeat) {
+        for (int i = 1; i <= repeat; i++) {
+            System.out.println("[기본 플레이어] (" + i + "/" + repeat + ")" + song);
+        }
+    }
+}
+
+class SmartMusicPlayer extends MusicPlayer {
+    public void play(String song) {
+        System.out.println("[스마트 플레이어] 고음질 재생: " + song);
+    }
+
+    public void play(String song, boolean shuffle) {
+        if (shuffle == true) {
+            System.out.println("[스마트 플레이어] 무작위 재생: " + song);
+        } else {
+            System.out.println("[스마트 플레이어] 순차 재생: " + song);
+        }
+    }
+}
+
 public class Overloading_and_Overriding_Challenge_2 {
-    
+    public static void main(String[]args) {
+        MusicPlayer basic = new MusicPlayer();
+        basic.play("Dynamite");
+        basic.play("Dynamite", 2);
+
+        System.out.println();
+
+        SmartMusicPlayer smart = new SmartMusicPlayer();
+        smart.play("Dynamite");
+        smart.play("Butter", true);
+        smart.play("Butter", 3);
+    }
 }
