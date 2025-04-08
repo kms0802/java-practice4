99a7c2ec04eeefddafca77738b4df0f577078b9d
kms0802 <kms0802@cju.ac.kr>
2025-04-08 22:52:18 +0900

    오버라이딩 오버로딩 문제 도전

---
+++ VSCode_Java/src/OPPs/Overloading_and_Overriding/Overloading_and_Overriding_Challenges/Overloading_and_Overriding_Challenge_1.java
@@ -0,0 +1,41 @@
+package OPPs.Overloading_and_Overriding.Overloading_and_Overriding_Challenges;
+
+class Messenger {
+    public void send(String message) {
+        System.out.println("[기본 메신저] 메세지 전송: " + message);
+    }
+
+    public void send(String message,int count) {
+        for (int i = 0; i <= count; i++) {
+            System.out.println("[기본 메신저] (" + i + "/" + count + ") 안녕");
+        }
+    }
+}
+
+class SmartMessenger extends Messenger {
+    public void send(String message) {
+        System.out.println("[스마트 메신저] 알림과 함께 메시지 전송: " + message);
+    }
+
+    public void send(String message, boolean isImportant) {
+        if (isImportant == true) {
+            System.out.println("[스마트 메신저] [긴급]" + message);
+        } else {
+            System.out.println("[스마트 메신저]" + message);
+        }
+    }
+}
+
+public class Overloading_and_Overriding_Challenge_1 {
+    public static void main(String[]args) {
+        Messenger m = new Messenger();
+        m.send("안녕");
+        m.send("안녕", 3);
+
+        System.out.println();
+
+        SmartMessenger sm = new SmartMessenger();
+        sm.send("안녕", true);
+        sm.send("반가워",2);
+    }
+}
