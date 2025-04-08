1c75876b0c39ac9f8bdc932413869e44e092d02d
kms0802 <kms0802@cju.ac.kr>
2025-04-08 23:57:01 +0900

    오버라이딩 오버로딩으로 카메라 스마트카메라 문제풀이

---
+++ VSCode_Java/src/OPPs/Overloading_and_Overriding/Overloading_and_Overriding_Challenges/Overloading_and_Overriding_Challenge_3.java
@@ -0,0 +1,40 @@
+package OPPs.Overloading_and_Overriding.Overloading_and_Overriding_Challenges;
+
+class Camera {
+    public void takePhoto() {
+        System.out.println("[기본 카메라] 사진을 찍었습니다. ");
+    }
+
+    public void takePhoto(String mode) {
+        System.out.println("[기본 카메라] " + mode + "모드로 사진을 찍었습니다. ");
+    }
+}
+
+class SmartCamera extends Camera {
+    public void takePhoto() {
+        System.out.println("[스마트 카메라] AI 인식후 사진을 찍었습니다.");
+    }
+
+    public void takePhoto(String mode, boolean isHDR) {
+        if (isHDR == true) {
+            System.out.println("[스마트 카메라] HDR + " + mode + " 모드로 촬영!");
+        } else {
+            System.out.println("[스마트 카메라] " + mode + " 모드로 일반 촬영");
+        }
+    }
+}
+
+public class Overloading_and_Overriding_Challenge_3 {
+    public static void main(String[]args) {
+        Camera basic = new Camera();
+        basic.takePhoto();
+        basic.takePhoto("야간");
+
+        System.out.println();
+
+        SmartCamera smart = new SmartCamera();
+        smart.takePhoto();
+        smart.takePhoto("야간", true);
+        smart.takePhoto("인물", false);
+    }
+}
