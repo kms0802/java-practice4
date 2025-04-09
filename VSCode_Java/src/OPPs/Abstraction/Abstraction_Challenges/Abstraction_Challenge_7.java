b0b6f6968d3a150fc3e9d9e59d20d5a2028f3013
kms0802 <kms0802@cju.ac.kr>
2025-04-09 23:57:54 +0900

    문제풀이 7 해답 수정인데 이래도 컴파일 오류

--- VSCode_Java/src/OPPs/Abstraction/Abstraction_Challenges/Abstraction_Challenge_7.java
+++ VSCode_Java/src/OPPs/Abstraction/Abstraction_Challenges/Abstraction_Challenge_7.java
@@ -21,20 +21,33 @@ class Manager extends Employee {
     }
 
     public int calculatePay() {
-        basePay = basePay + 1000000;
-        return basePay;
+        return basePay + 1000000; // 리턴값만 치면 되던거였다.
     }
 }
 
 class PartTimer extends Employee {
     int hoursWorked;
-    public PartTimer(String name, int basePay) {
-        hoursWorked = super(name, basePay) + this.hoursWorked;
+    public PartTimer(String name, int basePay, int hoursWorked) { // hoursWorked까지 선언했어야함.
+        super(name, basePay);
+        this.hoursWorked = hoursWorked; // 슈퍼랑 파트타임이랑 더하는게 아니였음.
     }
-
+    
+    @Override
+    public int calculatePay() {
+        return basePay * hoursWorked;
+    }
     
 }
 
 public class Abstraction_Challenge_7 {
-    
+    public static void main(String[]args) {
+        Employee e1 = new Manager("김과장", 3000000);
+        Employee e2 = new PartTimer("이알바", 10000, 120);
+
+        e1.printInfo();
+        System.out.println("실급여: " + e1.calculatePay());
+
+        e2.printInfo();
+        System.out.println("실급여: " + e2.calculatePay());
+    }
 }
