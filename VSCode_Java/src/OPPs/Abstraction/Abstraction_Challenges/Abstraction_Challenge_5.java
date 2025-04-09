1246d7819a6630ab6077d537be0a0789e920028b
kms0802 <kms0802@cju.ac.kr>
2025-04-09 23:07:25 +0900

    추상클래스에서
     - 생성자 이름은 클래스 이름과 정확히 일치해야 한다.
     - super()함수는 생성자 자체를 명시해주어야 사용이 가능하다. 예) public Car(String brand) {super(brand)};
     - 추상클래스는 오버라이딩이 필수

--- VSCode_Java/src/OPPs/Abstraction/Abstraction_Challenges/Abstraction_Challenge_5.java
+++ VSCode_Java/src/OPPs/Abstraction/Abstraction_Challenges/Abstraction_Challenge_5.java
@@ -2,7 +2,7 @@ package OPPs.Abstraction.Abstraction_Challenges;
 
 abstract class Vehicle_5 {
     String brand;
-    public Vehicle(String brand) {
+    public Vehicle_5(String brand) {
         this.brand = brand;
     }
 
@@ -14,19 +14,33 @@ abstract class Vehicle_5 {
 }
 
 class Car_5 extends Vehicle_5 {
-    super(brand);
-    public move() {
-        System.out.println("차가 도로를 달립니다.");
+    public Car_5(String brand) {
+        super(brand);
+    }
+    public void move() {
+        System.out.println("도로를 달립니다.");
     }
 }
 
 class Boat_5 extends Vehicle_5 {
-    super(brand);
-    public move() {
+    public Boat_5(String brand) {
+        super(brand);
+    }
+
+    public void move() {
         System.out.println("바다를 떠다닙니다.");
     }
 }
 
 public class Abstraction_Challenge_5 {
-    
+    public static void main(String[]args) {
+        Vehicle_5 v1 = new Car_5("현대");
+        Vehicle_5 v2 = new Boat_5("보트코리아");
+
+        v1.printBrand();
+        v1.move();
+
+        v2.printBrand();
+        v2.move();
+    }
 }
