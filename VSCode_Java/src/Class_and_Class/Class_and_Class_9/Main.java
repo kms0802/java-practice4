aade7eb861ad099b801659853aebeb397e728974
kms0802 <kms0802@cju.ac.kr>
2025-04-10 19:58:58 +0900

    포함예제9

---
+++ VSCode_Java/src/Class_and_Class/Class_and_Class_9/Main.java
@@ -0,0 +1,39 @@
+package Class_and_Class.Class_and_Class_9;
+
+class Laptop {
+    String brand;
+    int ramSize;
+
+    public Laptop(String brand, int ramSize) {
+        this.brand = brand;
+        this.ramSize = ramSize;
+    }
+
+    public void showSpecs() {
+        System.out.println("노트북 브랜드: " + brand + ", RAM: " + ramSize + "GB");
+    }
+}
+
+
+class Student {
+    String name;
+    Laptop laptop;
+
+    public Student(String name, Laptop laptop) {
+        this.name = name;
+        this.laptop = laptop;
+    }
+
+    public void study() {
+        System.out.println(name + "이(가) 공부를 시작합니다.");
+        laptop.showSpecs();
+    }
+}
+public class Main {
+    public static void main(String[]args) {
+        Laptop myLaptop = new Laptop("LG Gram", 16);
+        Student student = new Student("지민", myLaptop);
+
+        student.study();
+    }
+}
