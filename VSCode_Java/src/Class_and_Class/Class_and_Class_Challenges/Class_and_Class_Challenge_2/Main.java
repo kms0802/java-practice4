fb56307cc8d95ffb5365eca4b2479431281ec4af
kms0802 <kms0802@cju.ac.kr>
2025-04-10 20:20:38 +0900

    포함 문제풀이2

---
+++ VSCode_Java/src/Class_and_Class/Class_and_Class_Challenges/Class_and_Class_Challenge_2/Main.java
@@ -0,0 +1,39 @@
+package Class_and_Class.Class_and_Class_Challenges.Class_and_Class_Challenge_2;
+
+class Book {
+    String title;
+    String author;
+
+    public Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
+
+    public void printInfo() {
+        System.out.println("책 제목: " + title + ", 저자: "  + author);
+    }
+}
+
+class Library {
+    String name;
+    Book book;
+
+    public Library(String name, Book book) {
+        this.name = name;
+        this.book = book;
+    }
+
+    public void showLibraryInfo() {
+        System.out.println("도서관 이름: " + name);
+        book.printInfo();
+    }
+}
+
+public class Main {
+    public static void main(String[]args) {
+        Book book = new Book("자바의 정석", "남궁석");
+        Library library = new Library("중앙도서관", book);
+
+        library.showLibraryInfo();
+    }
+}
