91afa03ff9756d86b7dee96b9b91d4084a39744e
kms0802 <kms0802@cju.ac.kr>
2025-04-10 21:46:53 +0900

    패키지예제1

---
+++ VSCode_Java/src/Package/Main_1.java
@@ -0,0 +1,10 @@
+package Package;
+
+import Package.mypackage.Greeting;
+
+public class Main_1 {
+    public static void main(String[]args) {
+        Greeting g = new Greeting();
+        g.sayHello();
+    }
+}
---
+++ VSCode_Java/src/Package/mypackage/Greeting.java
@@ -0,0 +1,7 @@
+package Package.mypackage;
+
+public class Greeting {
+    public void sayHello() {
+        System.out.println("안녕하세요! 패키지 안에서 인사드립니다.");
+    }
+}
