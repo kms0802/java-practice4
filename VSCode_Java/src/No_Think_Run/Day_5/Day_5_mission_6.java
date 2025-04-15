abdee289ded9f8b5a6a6801baf04e5a91912f2ed
kms0802 <kms0802@cju.ac.kr>
2025-04-15 22:23:44 +0900

    .

--- VSCode_Java/src/No_Think_Run/Day_5/Day_5_mission_6.java
+++ VSCode_Java/src/No_Think_Run/Day_5/Day_5_mission_6.java
@@ -1,5 +1,20 @@
 package No_Think_Run.Day_5;
 
+@FunctionalInterface
+interface Calculator {
+    int operate(int a, int b);
+}
+
 public class Day_5_mission_6 {
-    
+    public static void main(String[]args) {
+        Calculator add = (a, b) -> a + b;
+        Calculator sub = (a, b) -> a - b;
+        Calculator gob = (a, b) -> a * b;
+        Calculator div = (a, b) -> b == 0 ? 0 : a / b;
+
+        System.out.println(add.operate(10, 2));
+        System.out.println(sub.operate(10, 2));
+        System.out.println(gob.operate(10, 2));
+        System.out.println(div.operate(10, 2));
+    }
 }
