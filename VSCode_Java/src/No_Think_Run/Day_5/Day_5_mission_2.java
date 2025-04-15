a4a9620e1316f9dee21f83d85069f10771e80eaf
kms0802 <kms0802@cju.ac.kr>
2025-04-15 21:49:39 +0900

    .

--- VSCode_Java/src/No_Think_Run/Day_5/Day_5_mission_2.java
+++ VSCode_Java/src/No_Think_Run/Day_5/Day_5_mission_2.java
@@ -1,5 +1,15 @@
 package No_Think_Run.Day_5;
 
+@FunctionalInterface
+interface Checker {
+    boolean check(int n);
+}
+
 public class Day_5_mission_2 {
-    
+    public static void main(String[]args) {
+        Checker isEven = (n) -> n % 2 == 0;
+
+        System.out.println("7은 짝수인가요? " + isEven.check(7));
+        System.out.println("10은 짝수인가요? " + isEven.check(10));
+    }
 }
