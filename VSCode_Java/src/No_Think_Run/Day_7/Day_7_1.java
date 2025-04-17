643b18a3291b92b340ffb234b5c5cdc4de145144
kms0802 <kms0802@cju.ac.kr>
2025-04-17 20:31:34 +0900

    예외처리1 ArithmeticException

---
+++ VSCode_Java/src/No_Think_Run/Day_7/Day_7_1.java
@@ -0,0 +1,17 @@
+package No_Think_Run.Day_7;
+
+public class Day_7_1 {
+    public static void main(String[]args) {
+        int num1 = 10;
+        int num2 = 0;
+
+        try {
+            int result = num1 / num2;
+            System.out.println("결과: " + result);
+        } catch (ArithmeticException e) {
+            System.out.println("예외 발생: 0으로 나눌 수 없습니다.");
+        }
+
+        System.out.println("프로그램이 정상적으로 종료되었습니다. ");
+    }
+}
