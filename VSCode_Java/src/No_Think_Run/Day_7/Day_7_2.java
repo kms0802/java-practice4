802232588a7c90a5db8d931d97ee3119d0ce194e
kms0802 <kms0802@cju.ac.kr>
2025-04-17 20:57:09 +0900

    InputMismatchException e 예외처리 예제2
    숫자가 아닌 값이면 InputMismatchException이 발생

--- VSCode_Java/src/No_Think_Run/Day_7/Day_7_2.java
+++ VSCode_Java/src/No_Think_Run/Day_7/Day_7_2.java
@@ -1,7 +1,28 @@
 package No_Think_Run.Day_7;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Day_7_2 {
     public static void main(String[]args) {
-        
+        Scanner scanner = new Scanner(System.in);
+
+        try {
+            System.out.println("첫 번째 숫자: ");
+            int num1 = scanner.nextInt();
+
+            System.out.println("두 번째 숫자: ");
+            int num2 = scanner.nextInt();
+
+            double result = (double) num1 / num2;
+            System.out.println("결과 값: " + result);
+        } catch (ArithmeticException e) {
+            System.out.println("예외 발생: 0으로 나눌 수 없습니다.");
+        } catch (InputMismatchException e) {
+            System.out.println("입력 형식 오류: 점수를 입력하세요");
+        } finally {
+            System.out.println("프로그램 종료");
+            scanner.close();
+        }
     }
 }
